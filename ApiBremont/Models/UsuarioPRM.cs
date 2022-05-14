a8c412c6e461ffc612ab6e382901e5537a53d4a1
marcosbremont <bremont123@hotmail.com>
2022-05-14 13:38:58 -0400

    SendReport

--- ApiBremont/Models/UsuarioPRM.cs
+++ ApiBremont/Models/UsuarioPRM.cs
@@ -32,5 +32,29 @@ namespace ApiBremont.Models
 
         }
 
+        public Result SendReport(string report)
+        {
+            Result result = new Result();
+            try
+            {
+                using (GetCon())
+                {
+                    MySqlCommand cmd = new MySqlCommand($"IReportPrm(?)", GetCon());
+                    cmd.Parameters.Add("prm_description", MySqlDbType.VarChar).Value = report;
+                    Conectar();
+                    cmd.ExecuteNonQuery();
+                    result.Respuesta = "OK";
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Respuesta = "ERROR";
+                result.Mensaje = ex.Message;
+            }
+
+            return result;
+
+        }
+
     }
 }
