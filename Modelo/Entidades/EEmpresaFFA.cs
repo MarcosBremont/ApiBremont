6fcec0d239ac51443d616401ffa3bdd37a7df2cd
Marcos Bremont <bremont123@hotmail.com>
2022-05-16 11:52:06 -0400

    .

--- Modelo/Entidades/EEmpresaFFA.cs
+++ Modelo/Entidades/EEmpresaFFA.cs
@@ -4,11 +4,20 @@ using System.Text;
 
 namespace Modelo.Entidades
 {
-    public class EMenu
+    public class EEmpresaFFA
     {
         public string nombre { get; set; }
-        public decimal precio { get; set; }
-        public string disponible { get; set; }
-        public string foto { get; set; }
+        public string rnc { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+        public string whatsapp { get; set; }
+        public string correo { get; set; }
+        public string latitud { get; set; }
+        public string longitud { get; set; }
+        public string clave { get; set; }
+        public string instagram { get; set; }
+        public string facebook { get; set; }
+        public string encargado_empresa { get; set; }
+        public string envio { get; set; }
     }
 }
