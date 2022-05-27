b1ccac3538d1fec7f14b5db5b5ddae88efd35d8d
Marcos Bremont <bremont123@hotmail.com>
2022-05-27 14:01:29 -0400

    `.

--- Modelo/Entidades/EPedidoTemporal.cs
+++ Modelo/Entidades/EPedidoTemporal.cs
@@ -8,6 +8,7 @@ namespace Modelo.Entidades
     {
         public int idpedido_temporal_fast_food { get; set; }
         public int id_pedido_general { get; set; }
+        public int idpedidos_fast_food { get; set; }
         public int idusuarios { get; set; }
         public int idmenu_fast_food { get; set; }
         public int cantidad { get; set; }
